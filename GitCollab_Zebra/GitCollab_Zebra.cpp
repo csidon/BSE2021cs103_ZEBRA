6c6b51960e795e5764b0a8e2a62c775756a10316
Chris Chong <270158235@yoobeestudent.ac.nz>
2021-10-21 09:42:13 +1300

    initial commit with headers

---
+++ GitCollab_Zebra/GitCollab_Zebra.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+//structure
+
+//function prototype 
+
+
+int main()
+{
+	//WE HAVE STARTED ZEBRAAAAAAA
+	cout << "Chris Has started code ZEBRA.";
+}
