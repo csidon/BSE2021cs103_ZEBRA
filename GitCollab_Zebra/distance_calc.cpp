fd7c6501973a4540d6ad724b30cc9ff7124b038c
ana <nastya.karpova123@gmail.com>
2021-11-11 10:23:31 +1300

    Calculation finished

--- GitCollab_Zebra/distance_calc.cpp
+++ GitCollab_Zebra/distance_calc.cpp
@@ -1,31 +1,17 @@
 #include <iostream>
 #include<cmath>
+#include "distance_calc.h"
 using namespace std;
 //Coder: Anastasiia K
 //File: distance_calc.cpp
-//Edited on: 2 Nov 2021
-//Last Edited Date:  2 Nov 2021
-//Purpose: This file contains the decorative functions for the Zebra program
+//Edited on: 10 Nov 2021
+//Last Edited Date:  11 Nov 2021
+//Purpose: This file contains the distance CALCULATION for the Zebra program
 
-struct Area {
-	double x;
-	double y;
-	string id;
 
-	Area(double x1, double y1, string id1)
-	{
-		x = x1;
-		y = y1;
-		id = id1;
-	}
-	Area()
-	{
-		x = 0.0;
-		y = 0.0;
-		id = "";
-	}
-};
-
+//*****************
+//FOLMULA FOR CALCULATION ON A COORDINATE PLANE
+//*****************
 double calc(struct Area p, struct Area ptr)
 {
 	double dist;
@@ -60,74 +46,92 @@ double calc(struct Area p, struct Area ptr)
 //	return (p);
 //}
 
+//*****************
+//MANUALY INPUTING COORDINATES USING POINTER
+//*****************
 
-struct Area* input_B(struct Area* ptr)
+struct Area* input(struct Area* ptr)
 {
-	ptr->id = "Brooklyn";
+	ptr->id = "BROOKLYN";
 	ptr->x = 1.0;
 	ptr->y = 1.0;
 	ptr++;
 
-	ptr->id = "Hataitai";
+	ptr->id = "HATAITAI";
 	ptr->x = 16.0;
 	ptr->y = 2.5;
 	ptr++;
 
-	ptr->id = "Aro_Valley";
+	ptr->id = "ARO_VALLEY";
 	ptr->x = 3.0;
 	ptr->y = 8.0;
 	ptr++;
 
-	ptr->id = "Kelburn";
+	ptr->id = "KELBURN";
 	ptr->x = 3.0;
 	ptr->y = 13.0;
 	ptr++;
 
-	ptr->id = "Te_Aro";
-	ptr->x = 8.0;
-	ptr->y = 8.0;
-	ptr++;
 
-	ptr->id = "Mt_Cook";
+	ptr->id = "MT_COOK";
 	ptr->x = 6.5;
 	ptr->y = 4.3;
 	ptr++;
+
+	ptr->id = "TE_ARO";
+	ptr->x = 8.0;
+	ptr->y = 8.0;
+
 	return (ptr);
 }
 
+//*****************
+//FUNCTION TO SET COORDINATES
+//*****************
+
 bool set_coordinates(struct Area data_array[], struct Area* coordinates, string input)
 {
-	int arrSize = *(&data_array + 1) - data_array;
+	int arrSize = *(&data_array + 1) - data_array;//CALCULATING SIZE OF THE ARRAY
 	for (int i = 0; i < arrSize; i++)
 	{
-		//cout << i << endl;
+		//cout << i << endl;//DEBUG
 		if (input == data_array[i].id)
 		{
 			//cout << "\nDEBUG";
 			coordinates->x = data_array[i].x;
 			coordinates->y = data_array[i].y;
 			coordinates->id = data_array[i].id;
-			//cout << "\n"<<coordinates->id<<endl;
+			//cout << "\n"<<coordinates->id<<endl;//DEBUG
 			return(true);
 		}
 	}
 	return (false);
 }
 
-int main()
+//*****************
+//MAIN FUNCTION
+//*****************
+
+double dist_calc_main()
 {
-	double a;
+	double dist;
+	string a, b;
 	Area coordinates_A, coordinates_B;
 	Area* p;
-	Area f[6];
-	p = f;
-	input_B(p);
+	Area arr[6];
+	p = arr;
+	input(p);
 	for (int i = 0; i < 6; i++)
 	{
-		cout << f[i].id << endl;
+		cout << arr[i].id << endl;
 	}
-	set_coordinates(f, &coordinates_A, "Hataitai");
-	set_coordinates(f, &coordinates_B, "Brooklyn");
-	a = calc(coordinates_A, coordinates_B)/2;
-	cout << a;
+	cout << "\n\tPlease enter a pick up location: ";
+	cin >> a;
+	cout << "\n\tPlease enter a drop off location: ";
+	cin >> b;
+	set_coordinates(arr, &coordinates_A, a);
+	set_coordinates(arr, &coordinates_B, b);
+	dist = calc(coordinates_A, coordinates_B)/2;
+	cout <<"\n\tThe distance between to points is: "<< dist<<" KM";
+	return (dist);
 }
