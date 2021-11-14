4057f66f137d713d4bfe270a8754d55a2ae30c3d
ana <nastya.karpova123@gmail.com>
2021-11-15 12:44:55 +1300

    Driver rides file manupalation

--- GitCollab_Zebra/zebra_driver.h
+++ GitCollab_Zebra/zebra_driver.h
@@ -69,6 +69,46 @@ struct Drivers
 	}
 };
 
+struct Driver_rides
+{
+
+	string r_uidalpha;
+	int rider_uidnum;
+	string r_pname;
+	string r_has_pay_details;
+	string r_phone_num;
+	string r_defaultloc;
+	string r_startloc;
+	string r_endloc;
+	double trip_cost;
+	double gst;
+	double net_trip_revenue;
+	string d_pref_name;
+	string d_veh_model;
+	string d_veh_regist;
+	string d_phone_num;
+	Driver_rides(string ruda = "", int rudn = 0, string rpn = "", string rhpd = "", string rphn = "", string rdl = "", string rsl = "", string rel = "", double ts = 0, double gstt = 0, double ntr = 0, string dpn = "", string dvm = "", string dvr = "", string dphn = "")
+	{
+		r_uidalpha= ruda;
+		rider_uidnum= rudn;
+		r_pname= rpn;
+		r_has_pay_details= rhpd;
+		r_phone_num= rphn;
+		r_defaultloc= rdl;
+		r_startloc= rsl;
+		r_endloc= rel;
+		trip_cost= ts;
+		gst= gstt;
+		net_trip_revenue= ntr;
+		d_pref_name= dpn;
+		d_veh_model= dvm;
+		d_veh_regist= dvr;
+		d_phone_num= dphn;
+
+	}
+};
+
+
 
 //###############################################
 //		Function Prototype
@@ -83,11 +123,13 @@ int count_entries_drive();
 void writeToFile(vector<Drivers>& driver);//returns nothing
 vector <Drivers> readFromFile();
 
-//vector<Drivers> update_acc_details(vector < Drivers > driverFromFile);
 vector<Drivers> update_acc_details(vector < Drivers > driverFromFile, string check_username);
 
 void output_drivers(vector<Drivers>& driver);
 void driver_account_main(vector<Drivers>& driverFromFile, string check_username);
 void account_settings(vector<Drivers>& driverFromFile, string check_username);
+//vector <Driver_rides> job_screen(vector <Driver_rides> driver_rides);
+
+//vector <Driver_rides> read_from_driverRides();
 
 #endif
